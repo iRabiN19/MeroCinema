927bb53937874b4a392e157c3191e46040fcff2a
Rabin Maharjan <jravinmaharjan@gmail.com>
2023-06-18 21:11:04 +0545

    Database migrations successfull

---
+++ app/Http/Controllers/ProducerController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Producer;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProducerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Producer $producer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Producer $producer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Producer $producer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Producer $producer)
+    {
+        //
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,13 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('user', function (Blueprint $table) {
+            $table->increments('u_id');
+            $table->string('u_Fname', 30);
+            $table->string('u_Mname', 30);
+            $table->string('u_Lname', 30);
+            $table->string('u_email', 40);
+            $table->string('u_password', 30);
+            $table->date('date_of_birth');
+            $table->string('u_phoneNo', 15);
+            $table->string('u_username', 30)->unique();
             $table->timestamps();
         });
     }
